5393f7d64b1a367d341bf01b6d3084b6589bbddb
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-20 16:20:52 -0400

    fixed and modified the create method

--- controller.rb
+++ controller.rb
@@ -5,6 +5,7 @@
 # # CONTROLLER:
 # The controller will gather data from the cookbook to hand them over to the view.
 # It will also ask the view for information to create new recipes.
+require_relative 'recipe_model'
 
 class Controller # holds reference to the repo and the view
   def initialize(cookbook)
@@ -13,13 +14,14 @@ class Controller # holds reference to the repo and the view
    end
 
   def list
-    info_to_display = @cookbook.all #return the array from the repo
-    @view.display(info_to_display) #send it to the view to display
+    info_to_display = @cookbook.all # return the array from the repo
+    @view.display(info_to_display) # send it to the view to display
    end
 
-  def create #needs to take a "recipe" from the view...in order to pass to the repo
-  user_input = @view.get_new #this will be a hash,created in the view...now send that to repo
-@cookbook.add_recipe(user_input)
+  def create # needs to take a "recipe" from the view...in order to pass to the repo
+    user_input = @view.get_new # this will be a hash,created in the view...now send that to repo
+   created_recipe = Recipe.new(user_input[0], user_input[1])
+    @cookbook.add_recipe(created_recipe)
   end
 
   def destroy; end
--- view.rb
+++ view.rb
@@ -3,16 +3,16 @@
 class View
   def display(info_from_repo)
     info_from_repo.each_with_index do |item, index|
-      puts "#{index + 1}: #{item}"
+      puts "#{index + 1}: #{item.name}"
     end
    end
 
   def get_new
-  puts "Pick a name for your recipe"
-  recipe_name = gets.chomp
-  puts "O.K buddy, describe your recipe in a few words"
-  description = gets.chomp
-  puts "Good choice, I just added your reicpe to the cookbook"
-  return {:name => recipe_name, :description => description}
+    puts 'Pick a name for your recipe'
+    recipe_name = gets.chomp
+    puts 'O.K buddy, describe your recipe in a few words'
+    description = gets.chomp
+    puts 'Good choice, I just added your reicpe to the cookbook'
+    return new_val = [recipe_name, description]
   end
 end

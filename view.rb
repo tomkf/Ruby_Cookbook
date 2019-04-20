312070d1b84d5eb5a71463ccce6a59d0b35ec6e1
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-20 15:56:34 -0400

    fix

--- view.rb
+++ view.rb
@@ -1,5 +1,3 @@
-# VIEW:
-# The view is responsible for all the puts and gets of your code.
 # CONTROLLER HOLDS REFERENCE TO THIS
 
 class View
@@ -7,6 +5,7 @@ class View
   def display(info_from_repo)
     info_from_repo.each do |item|
       puts item
+    end
    end
 
 

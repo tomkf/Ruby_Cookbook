4762977307ead11b5b13c596aed8b7d0e5c829f1
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-20 17:14:34 -0400

    now can add to CSV file

--- controller.rb
+++ controller.rb
@@ -1,25 +1,18 @@
-#  It will also ask the view for information to create new recipes. (instance methods)
-
-# The controller will gather data from the repo to hand them over to the view.
-
-# # CONTROLLER:
-# The controller will gather data from the cookbook to hand them over to the view.
-# It will also ask the view for information to create new recipes.
 require_relative 'recipe_model'
 
-class Controller # holds reference to the repo and the view
+class Controller 
   def initialize(cookbook)
     @cookbook = cookbook
     @view = View.new
    end
 
   def list
-    info_to_display = @cookbook.all # return the array from the repo
-    @view.display(info_to_display) # send it to the view to display
+    info_to_display = @cookbook.all 
+    @view.display(info_to_display) 
    end
 
-  def create # needs to take a "recipe" from the view...in order to pass to the repo
-    user_input = @view.get_new # this will be a hash,created in the view...now send that to repo
+  def create 
+    user_input = @view.get_new 
    created_recipe = Recipe.new(user_input[0], user_input[1])
     @cookbook.add_recipe(created_recipe)
   end
--- repo.rb
+++ repo.rb
@@ -1,20 +1,33 @@
+require_relative 'recipe_model'
+
 class Cookbook
   attr_reader :local_storage, :csv_file_path
   attr_accessor :local_storage, :csv_file_path
   def initialize(csv_file_path = nil)
     @csv_file_path = csv_file_path
     @local_storage = []
+    CSV.foreach(@csv_file_path) do |row|  #write to the local storage at start...fill array
+    @local_storage.push(Recipe.new(row[0], row[1]))
+    end
    end
 
   def all
-    @local_storage
+    @local_storage  
   end
 
-  def add_recipe(recipe)
+  def add_recipe(recipe)  #controller will give an instance obj. so write to CSV with this
     @local_storage.push(recipe)
+      CSV.open(@csv_file_path, "a") do |csv|
+        csv << [recipe.name, recipe.description]
+    end
   end
 
-  def remove_recipe(recipe_index)
+  def remove_recipe(recipe_index) #controller will give an instance obj.
+    if csv_file_path == nil
     @local_storage.delete_at(recipe_index)
+   else 
+
+   end
   end
  end
+

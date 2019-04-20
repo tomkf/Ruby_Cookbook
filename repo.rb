0a2d11e86b68f97ceb9f3b20d96ff26de89ef096
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-20 17:40:07 -0400

    Update repo.rb

--- repo.rb
+++ repo.rb
@@ -6,7 +6,7 @@ class Cookbook
   def initialize(csv_file_path = nil)
     @csv_file_path = csv_file_path
     @local_storage = []
-    CSV.foreach(@csv_file_path) do |row|  #write to the local storage at start...fill array
+    CSV.foreach(@csv_file_path) do |row|  
     @local_storage.push(Recipe.new(row[0], row[1]))
     end
    end
@@ -15,14 +15,14 @@ class Cookbook
     @local_storage  
   end
 
-  def add_recipe(recipe)  #controller will give an instance obj. so write to CSV with this
+  def add_recipe(recipe)  
     @local_storage.push(recipe)
       CSV.open(@csv_file_path, "a") do |csv|
         csv << [recipe.name, recipe.description]
     end
   end
 
-  def remove_recipe(recipe_index) #controller will give an instance obj.
+  def remove_recipe(recipe_index) 
     if csv_file_path == nil
     @local_storage.delete_at(recipe_index)
    else 

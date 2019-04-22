42b50a31496a444d2122460a1479edadc3351c36
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-22 17:22:37 -0400

    working w/ re-writing csv

--- repo.rb
+++ repo.rb
@@ -6,29 +6,37 @@ class Cookbook
   def initialize(csv_file_path = nil)
     @csv_file_path = csv_file_path
     @local_storage = []
-    CSV.foreach(@csv_file_path) do |row|  
-    @local_storage.push(Recipe.new(row[0], row[1]))
-    end
+    load_csv
    end
 
-  def all
-    @local_storage  
-  end
-
-  def add_recipe(recipe)  
-    @local_storage.push(recipe)
-      CSV.open(@csv_file_path, "a") do |csv|
-        csv << [recipe.name, recipe.description]
+  def load_csv
+    CSV.foreach(@csv_file_path) do |row|
+      @local_storage.push(Recipe.new(row[0], row[1], row[2], row[3]))
     end
   end
 
-  def remove_recipe(recipe_index) 
+  def all
+    @local_storage
+  end
+
+  def add_recipe(recipe)
+    @local_storage.push(recipe)
+    CSV.open(@csv_file_path, "a") do |csv|
+      csv << [recipe.name, recipe.description, recipe.prep_time, recipe.box_check]
+    end
+  end
+
+  def remove_recipe(recipe_index)
     @local_storage.delete_at(recipe_index)
     CSV.open(@csv_file_path, "w") do |csv|
       @local_storage.each do |item|
-        csv << [item.name, item.description]
+        csv << [item.name, item.description, item.prep_time, item.box_check]
       end
     end
   end
- end
 
+  def write_to_csv
+    # call this maybe in the remove_recipe def....
+    CSV.open(@csv_file_path)
+      end
+ end

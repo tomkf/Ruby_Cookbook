84da5d9f44101732b9cdebd5ebe5129603b07367
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-22 17:21:57 -0400

    update state

--- recipe_model.rb
+++ recipe_model.rb
@@ -1,7 +1,14 @@
 class Recipe
-  attr_reader :name, :description
-  def initialize(name, description)
+  attr_reader :name, :description, :prep_time, :box_check
+  attr_writer :box_check
+  def initialize(name, description, prep_time, box_check = false)
     @name = name
     @description = description
+    @prep_time = prep_time
+    @box_check = box_check
+  end
+
+  def check
+    @box_check = true
   end
 end
